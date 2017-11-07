901aca4d19c989301737d488995e3fd8d72bdd4e
Andrew Hunter <andrew@logicalshift.co.uk>
2017-11-07 20:00:21 +0000

    Not sure this timeout is completely necessary?

--- tests/scheduler/thread_management.rs
+++ tests/scheduler/thread_management.rs
@@ -5,15 +5,13 @@ use super::timeout::*;
 #[test]
 fn will_despawn_extra_threads() {
     // As we join with the threads, we'll timeout if any of the spawned threads fail to end
-    timeout(|| {
-        let scheduler = scheduler();
+    let scheduler = scheduler();
 
-        // Maximum of 10 threads, but we'll spawn 20
-        scheduler.set_max_threads(10);
-        for _ in 1..20 {
-            scheduler.spawn_thread();
-        }
+    // Maximum of 10 threads, but we'll spawn 20
+    scheduler.set_max_threads(10);
+    for _ in 1..20 {
+        scheduler.spawn_thread();
+    }
 
-        scheduler.despawn_threads_if_overloaded();
-    }, 30000);
+    scheduler.despawn_threads_if_overloaded();
 }
