8bd552f6ee39a0c86e7af39d3f33752e7a69d09e
Andrew Hunter <andrew@logicalshift.co.uk>
2018-07-21 15:58:35 +0100

    Test pipe_in with a mpsc receiver

--- tests/pipe.rs
+++ tests/pipe.rs
@@ -3,8 +3,12 @@ extern crate futures;
 
 use desync::*;
 use futures::stream;
+use futures::executor;
+use futures::sync::mpsc;
 
 use std::sync::*;
+use std::thread;
+use std::time::Duration;
 
 #[test]
 fn pipe_in_simple_stream() {
@@ -20,4 +24,28 @@ fn pipe_in_simple_stream() {
 
     // Once the stream is drained, the core should contain Ok(1), Ok(2), Ok(3)
     assert!(obj.sync(|core| core.clone()) == vec![Ok(1), Ok(2), Ok(3)])
+}
+
+#[test]
+fn pipe_in_mpsc_receiver() {
+    // Create a channel to send to the object
+    let (sender, receiver) = mpsc::channel(10);
+
+    // Create an object
+    let obj = Arc::new(Desync::new(vec![]));
+
+    // Add anything received to the vector via a pipe
+    pipe_in(Arc::clone(&obj), receiver, |core: &mut Vec<i32>, item| core.push(item.unwrap()));
+
+    // Initially empty
+    assert!(obj.sync(|core| core.clone()) == vec![]);
+
+    // Send some values
+    let mut sender = executor::spawn(sender);
+    sender.wait_send(1).unwrap();
+    sender.wait_send(2).unwrap();
+
+    // Should arrive after a short delay
+    thread::sleep(Duration::from_millis(10));
+    assert!(obj.sync(|core| core.clone()) == vec![1, 2]);
 }
