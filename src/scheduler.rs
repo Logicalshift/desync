dc943b063f460349a16e861d9f89f5db4b2be53f
Andrew Hunter <andrew@logicalshift.co.uk>
2019-12-04 23:05:29 +0000

    Fix up some more usages of the Future trait

--- src/scheduler.rs
+++ src/scheduler.rs
@@ -867,7 +867,7 @@ pub fn desync<TFn: 'static+Send+FnOnce() -> ()>(queue: &Arc<JobQueue>, job: TFn)
 ///
 /// Schedules a job to run and returns a future for retrieving the result
 ///
-pub fn future<TFn, Item: 'static+Send>(queue: &Arc<JobQueue>, job: TFn) -> impl Future<Item=Item, Error=oneshot::Canceled>+Send
+pub fn future<TFn, Item: 'static+Send>(queue: &Arc<JobQueue>, job: TFn) -> impl Future<Output=Result<Item, oneshot::Canceled>>+Send
 where TFn: 'static+Send+FnOnce() -> Item {
     scheduler().future(queue, job)
 }
