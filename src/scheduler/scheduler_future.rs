bb8391fbcd7f02e21c1650289394ba4c4d1c4f91
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-22 10:57:07 +0000

    Fix typo

--- src/scheduler/scheduler_future.rs
+++ src/scheduler/scheduler_future.rs
@@ -196,7 +196,7 @@ impl<T> SchedulerFutureSignaller<T> {
     ///
     pub (super) fn signal(self, result: T) {
         let waker = {
-            let mut future_result = self.0.lock().expect("Scheduler futuer result");
+            let mut future_result = self.0.lock().expect("Scheduler future result");
 
             // Set the result
             future_result.result = FutureResultState::Some(Ok(result));
