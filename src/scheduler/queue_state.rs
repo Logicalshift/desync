06c7def7ecc34ac6c9a1137d411b36fb61e0e5e4
Andrew Hunter <andrew@logicalshift.co.uk>
2020-03-22 15:51:58 +0000

    `WaitingForPoll` is like `WaitingForUnpark` in that it indicates that while the queue is idle it's still 'running' as it can only be awoken safely by calling 'poll' on it again.

--- src/scheduler/queue_state.rs
+++ src/scheduler/queue_state.rs
@@ -4,24 +4,48 @@
 #[derive(PartialEq, Debug, Clone, Copy)]
 pub (super) enum QueueState {
     /// Queue is currently not running and not ready to run
+    /// 
+    /// The queue has this state when it has no jobs in it.
     Idle,
 
     /// Queue has been queued up to run but isn't running yet
+    /// 
+    /// In general, this means the queue is waiting for a background thread to run on.
     Pending,
 
     /// Queue has been assigned to a thread and is currently running
+    /// 
+    /// The queue must not be scheduled anywhere else while this is going on.
     Running,
 
     /// A job on the queue has indicated that it's waiting to be re-awakened (by the scheduler)
+    /// 
+    /// This state is reached when a future for this queue generates a result and the queue moves
+    /// into the pending state. It generally means it's scheduled to resume on a background
+    /// thread (similar to Pending)
     WaitingForWake,
 
     /// The queue is running synchronously on a thread and is waiting to be unparked
+    /// 
+    /// This occurs when the queue has been running on a thread and needs to wait for an event to
+    /// occur on some other thread, usually the completion of a future. Queues in this state must
+    /// only be resumed by the appropriate unparking method (the queue is effectively 'running'
+    /// on the thread where it is parked)
     WaitingForUnpark,
 
     /// We've returned from a polling operation and are waiting to be resumed
+    /// 
+    /// This happens when a queue has scheduled itself onto a thread as a result of calling the
+    /// poll() method on a future and has returned pending. This queue should be resumed when the
+    /// future is next awoken by calling 'poll' (it's effectively 'running' in the context of the
+    /// call too poll)
     WaitingForPoll,
 
     /// A wake-up call was made while the queue was in the running state
+    /// 
+    /// When attempting to suspend a queue as the result of a future indicating that it is pending,
+    /// it is possible for the future to wake the queue before it has reached the suspended state.
+    /// We capture this event by setting the queue's status to 'AwokenWhileRunning'.
     AwokenWhileRunning,
 
     /// Queue received a panic and is no longer able to be scheduled
@@ -36,6 +60,7 @@ impl QueueState {
         match self {
             QueueState::Running             | 
             QueueState::AwokenWhileRunning  | 
+            QueueState::WaitingForPoll      |
             QueueState::WaitingForUnpark    => true,
             _other                          => false
         }
