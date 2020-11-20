8070f5259dbc455a1c2dfcd7fe1360d15399d0f9
Andrew Hunter <andrew@logicalshift.co.uk>
2020-11-20 20:40:35 +0000

    Retry when changing to 'WaitingForScheduler' mode

--- src/scheduler/future_sync.rs
+++ src/scheduler/future_sync.rs
@@ -73,84 +73,98 @@ where   TFn:                Unpin+Send+FnOnce() -> TFuture,
         use self::SyncFutureState::*;
 
         // Rust doesn't seem to have a way to let us update the state in-place, so we need to swap out the old state and swap in the new state
-        let result;
+        let mut result;
         let mut state = Completed;
         mem::swap(&mut state, &mut self.state);
 
         // Update the state now we own it
-        state = match state {
-            WaitingForQueue(mut recv, create_future) => {
-                // Check the scheduler future (give it a chance to steal the thread)
-                if let Poll::Ready(Err(_)) = self.scheduler_future.poll_unpin(context) {
-                    // The queue will never get to the point of polling this future
-                    result = Poll::Ready(Err(oneshot::Canceled));
-                    self.task_finished.take().map(|finished| finished.send(()));
-                    Completed
-                } else {
-                    // Poll the receiver
-                    match recv.poll_unpin(context) {
-                        Poll::Ready(Ok(())) => {
-                            // Start the future
-                            let mut future = create_future();
+        loop {
+            // Set to true if a 'pending' result should trigger another state update (eg, if we start a new future, we should poll it)
+            let mut retry = false;
 
-                            // Poll it immediately to determine its status
-                            if let Poll::Ready(future_result) = future.poll_unpin(context) {
-                                // Future has completed
-                                result = Poll::Pending;
-                                self.task_finished.take().map(|finished| finished.send(()));
-                                WaitingForScheduler(Box::new(future_result))
-                            } else {
-                                // Future is still running
-                                result = Poll::Pending;
-                                WaitingForFuture(future)
+            state = match state {
+                WaitingForQueue(mut recv, create_future) => {
+                    // Check the scheduler future (give it a chance to steal the thread)
+                    if let Poll::Ready(Err(_)) = self.scheduler_future.poll_unpin(context) {
+                        // The queue will never get to the point of polling this future
+                        result = Poll::Ready(Err(oneshot::Canceled));
+                        self.task_finished.take().map(|finished| finished.send(()));
+                        Completed
+                    } else {
+                        // Poll the receiver
+                        match recv.poll_unpin(context) {
+                            Poll::Ready(Ok(())) => {
+                                // Start the future
+                                let mut future = create_future();
+
+                                // Poll it immediately to determine its status
+                                if let Poll::Ready(future_result) = future.poll_unpin(context) {
+                                    // Future has completed
+                                    result = Poll::Pending;
+                                    self.task_finished.take().map(|finished| finished.send(()));
+
+                                    retry = true;
+                                    WaitingForScheduler(Box::new(future_result))
+                                } else {
+                                    // Future is still running
+                                    result = Poll::Pending;
+                                    WaitingForFuture(future)
+                                }
                             }
-                        }
 
-                        Poll::Ready(Err(_)) => {
-                            // Future never became ready
-                            result = Poll::Ready(Err(oneshot::Canceled));
-                            Completed
-                        }
+                            Poll::Ready(Err(_)) => {
+                                // Future never became ready
+                                result = Poll::Ready(Err(oneshot::Canceled));
+                                Completed
+                            }
 
-                        Poll::Pending => {
-                            // Waiting for the queue to start this future
-                            result = Poll::Pending;
-                            WaitingForQueue(recv, create_future)
+                            Poll::Pending => {
+                                // Waiting for the queue to start this future
+                                result = Poll::Pending;
+                                WaitingForQueue(recv, create_future)
+                            }
                         }
                     }
                 }
-            }
 
-            WaitingForFuture(mut future) => {
-                if let Poll::Ready(future_result) = future.poll_unpin(context) {
-                    // Future has completed
-                    result = Poll::Pending;
-                    self.task_finished.take().map(|finished| finished.send(()));
-                    WaitingForScheduler(Box::new(future_result))
-                } else {
-                    // Future is still running
-                    result = Poll::Pending;
-                    WaitingForFuture(future)
+                WaitingForFuture(mut future) => {
+                    if let Poll::Ready(future_result) = future.poll_unpin(context) {
+                        // Future has completed
+                        result = Poll::Pending;
+                        self.task_finished.take().map(|finished| finished.send(()));
+
+                        retry = true;
+                        WaitingForScheduler(Box::new(future_result))
+                    } else {
+                        // Future is still running
+                        result = Poll::Pending;
+                        WaitingForFuture(future)
+                    }
                 }
-            }
 
-            WaitingForScheduler(future_result) => {
-                // Poll until the scheduler has finished running the task entirely (can deadlock while draining if we don't)
-                if let Poll::Ready(_) = self.scheduler_future.poll_unpin(context) {
-                    result = Poll::Ready(Ok(*future_result));
+                WaitingForScheduler(future_result) => {
+                    // Poll until the scheduler has finished running the task entirely (can deadlock while draining if we don't)
+                    if let Poll::Ready(_) = self.scheduler_future.poll_unpin(context) {
+                        result = Poll::Ready(Ok(*future_result));
+                        Completed
+                    } else {
+                        result = Poll::Pending;
+                        WaitingForScheduler(future_result)
+                    }
+                }
+
+                Completed => {
+                    // Polling after the result has been returned
+                    result = Poll::Ready(Err(oneshot::Canceled));
                     Completed
-                } else {
-                    result = Poll::Pending;
-                    WaitingForScheduler(future_result)
                 }
-            }
+            };
 
-            Completed => {
-                // Polling after the result has been returned
-                result = Poll::Ready(Err(oneshot::Canceled));
-                Completed
+            // Finish once retry is left at false
+            if !retry {
+                break;
             }
-        };
+        }
 
         // Swap the state back into the structure
         self.state = state;
